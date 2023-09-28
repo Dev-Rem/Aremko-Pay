3118d3b703fac188250847d42b739352b01c8539
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2023-09-28 08:43:51 +0100

    Initial Commit

---
+++ Aremko Pay/Aremko_PayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aremko_PayApp.swift
+//  Aremko Pay
+//
+//  Created by Aremu Oluwaseyi Festus on 2023-09-28.
+//
+
+import SwiftUI
+
+@main
+struct Aremko_PayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
